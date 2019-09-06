25fc1791ec5e314f4711cd63cdb0877897f31add
jimbucc <jimbucc@sbcglobal.net>
2019-09-06 05:23:30 -0400

    Updates

---
+++ precache-manifest.27b1e17c57710cc3ced3600833490eca.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9698a9b340e69ef04dd0988ea0d5c628",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "f4fee41a4dbbee2a1a8b",
+    "url": "/monsters-rolodex/static/css/main.ad5befc2.chunk.css"
+  },
+  {
+    "revision": "3751a725bd09b43602b0",
+    "url": "/monsters-rolodex/static/js/2.3dd6c484.chunk.js"
+  },
+  {
+    "revision": "f4fee41a4dbbee2a1a8b",
+    "url": "/monsters-rolodex/static/js/main.41b4589d.chunk.js"
+  },
+  {
+    "revision": "3eb51d48b619f371fccd",
+    "url": "/monsters-rolodex/static/js/runtime~main.877b8247.js"
+  }
+]);
